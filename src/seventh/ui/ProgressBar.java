34b9efb845cf2da71642d04259c9b203287e8370
tonysparks <anthony.sparks31@gmail.com>
2014-05-23 22:14:02 -0500

    Added progress bar for planting/disarming bombs

---
+++ src/seventh/ui/ProgressBar.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package seventh.ui;
+
+import leola.frontend.listener.EventDispatcher;
+
+/**
+ * A progress bar
+ * 
+ * @author Tony
+ *
+ */
+public class ProgressBar extends Widget {
+
+	public static final int MAX_PROGRESS = 100;
+	private int progress;
+	
+	public ProgressBar() {
+		this(new EventDispatcher());
+	}
+	
+	/**
+	 * @param eventDispatcher
+	 */
+	public ProgressBar(EventDispatcher eventDispatcher) {
+		super(eventDispatcher);
+	}
+
+	/**
+	 * The progress is from a scale of 0-100
+	 * 
+	 * @param progress the progress to set
+	 */
+	public void setProgress(int progress) {
+		this.progress = progress;
+		if(this.progress < 0) {
+			this.progress = 0;
+		}
+		if(this.progress > MAX_PROGRESS) {
+			this.progress = MAX_PROGRESS;
+		}
+	}
+	
+	/**
+	 * @return the progress
+	 */
+	public int getProgress() {
+		return progress;
+	}
+	
+	/**
+	 * @return the percentage completed
+	 */
+	public float getPercentCompleted() {
+		return (float)progress / (float)MAX_PROGRESS;
+	}
+}
---
+++ src/seventh/ui/view/ProgressBarView.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package seventh.ui.view;
+
+import seventh.client.gfx.Camera;
+import seventh.client.gfx.Canvas;
+import seventh.client.gfx.Renderable;
+import seventh.math.Rectangle;
+import seventh.shared.TimeStep;
+import seventh.ui.ProgressBar;
+
+/**
+ * The View of a {@link ProgressBar}
+ * 
+ * @author Tony
+ *
+ */
+public class ProgressBarView implements Renderable {
+
+	private ProgressBar progressBar;
+	
+	/**
+	 * 
+	 */
+	public ProgressBarView(ProgressBar progressBar) {
+		this.progressBar = progressBar;
+	}
+
+	/* (non-Javadoc)
+	 * @see seventh.client.gfx.Renderable#update(seventh.shared.TimeStep)
+	 */
+	@Override
+	public void update(TimeStep timeStep) {
+	}
+
+	/* (non-Javadoc)
+	 * @see seventh.client.gfx.Renderable#render(seventh.client.gfx.Canvas, seventh.client.gfx.Camera, long)
+	 */
+	@Override
+	public void render(Canvas canvas, Camera camera, long alpha) {
+		if(progressBar.isVisible()) {
+			Rectangle bounds = progressBar.getBounds();
+			canvas.fillRect(bounds.x, bounds.y, bounds.width, bounds.height, progressBar.getBackgroundColor());
+					
+			int percentOfWidth = (int)(progressBar.getPercentCompleted() * bounds.width);			
+			canvas.fillRect(bounds.x, bounds.y, percentOfWidth, bounds.height, progressBar.getForegroundColor());
+			canvas.drawRect(bounds.x, bounds.y, bounds.width, bounds.height, 0xff000000);
+			
+			int x = bounds.x;
+			int y = bounds.y;
+						
+			// add a shadow effect
+			canvas.drawLine( x, y+1, x+bounds.width, y+1, 0x8f000000 );
+			canvas.drawLine( x, y+2, x+bounds.width, y+2, 0x5f000000 );
+			canvas.drawLine( x, y+3, x+bounds.width, y+3, 0x2f000000 );
+			canvas.drawLine( x, y+4, x+bounds.width, y+4, 0x0f000000 );
+			canvas.drawLine( x, y+5, x+bounds.width, y+5, 0x0b000000 );
+			canvas.drawLine( x, y+6, x+bounds.width, y+6, 0x0a000000 );
+			
+			y = y+15;
+			canvas.drawLine( x, y-6, x+bounds.width, y-6, 0x0a000000 );
+			canvas.drawLine( x, y-5, x+bounds.width, y-5, 0x0b000000 );
+			canvas.drawLine( x, y-4, x+bounds.width, y-4, 0x0f000000 );
+			canvas.drawLine( x, y-3, x+bounds.width, y-3, 0x2f000000 );
+			canvas.drawLine( x, y-2, x+bounds.width, y-2, 0x5f000000 );
+			canvas.drawLine( x, y-1, x+bounds.width, y-1, 0x8f000000 );	
+		}
+	}
+
+}
