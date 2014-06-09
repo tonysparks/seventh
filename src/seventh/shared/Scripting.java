2674591bda3050a13a6a4820c2e97d6ec1ca8e52
tonysparks <anthony.sparks31@gmail.com>
2014-06-09 18:41:03 -0500

    Leola sandboxed mode

---
+++ src/seventh/shared/Scripting.java
@@ -0,0 +1,49 @@
+/*
+ * see license.txt 
+ */
+package seventh.shared;
+
+import leola.vm.Args;
+import leola.vm.Args.ArgsBuilder;
+import leola.vm.Leola;
+
+/**
+ * Scripting runtime factory methods
+ * 
+ * @author Tony
+ *
+ */
+public class Scripting {
+
+	
+	/**
+	 * Creates a new {@link Leola} runtime that is in sandboxed mode.
+	 * 
+	 * @return the runtime
+	 * @throws Exception
+	 */
+	public static Leola newSandboxedRuntime() throws Exception {
+		Args args = new ArgsBuilder()
+						.setAllowThreadLocals(false)
+						.setBarebones(true)
+						.setSandboxed(true)
+						.build();
+		
+		Leola runtime = new Leola(args);
+		return runtime;
+	}
+	
+	
+	/**
+	 * Creates a new {@link Leola} runtime that is not in sandboxed mode.
+	 *  
+	 * @return the runtime
+	 * @throws Exception
+	 */
+	public static Leola newRuntime() throws Exception {
+		Args leolaArgs = new ArgsBuilder().setAllowThreadLocals(false).build();		
+		Leola runtime = new Leola(leolaArgs);
+		return runtime;
+	}
+
+}
