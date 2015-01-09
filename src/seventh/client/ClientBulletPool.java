c1ee64e4ff7b8458ffb39bacba1993c0f4d67952
tonysparks <anthony.sparks31@gmail.com>
2015-01-08 20:11:43 -0600

    Reduced memory consumption

---
+++ src/seventh/client/ClientBulletPool.java
@@ -0,0 +1,76 @@
+/*
+ * see license.txt 
+ */
+package seventh.client;
+
+import seventh.math.Vector2f;
+
+/**
+ * Cache of {@link ClientBullet}s.
+ * 
+ * @author Tony
+ *
+ */
+public class ClientBulletPool {
+
+	private ClientBullet[] bullets;
+	
+	/**
+	 * @param maxEntities
+	 */
+	public ClientBulletPool(ClientGame game, int maxEntities) {
+		this.bullets = new ClientBullet[maxEntities];
+		for(int i = 0; i < maxEntities; i++) {
+			this.bullets[i] = new ClientBullet(game, new Vector2f());
+			this.bullets[i].id = i;
+		}
+	}
+		
+	/**
+	 * @param id
+	 * @return the {@link ClientBullet} at the supplied position or null if not available
+	 */
+	public ClientBullet alloc(int id, Vector2f pos) {
+		if(id > -1 && id < this.bullets.length) {
+			ClientBullet bullet = this.bullets[id];
+			bullet.reset();
+			bullet.setOrigin(pos);
+			
+			return bullet;
+		}
+		return null;
+	}
+	
+	/**
+	 * @param id
+	 * @return true if the entity exists in this list
+	 */
+	public boolean isFree(int id) {
+		if(id > -1 && id < this.bullets.length) {			
+			return this.bullets[id] != null && this.bullets[id].isDestroyed();
+		}
+		return false; 
+	}
+	
+	/**
+	 * Adds the entity
+	 * @param e
+	 */
+	public void free(ClientBullet e) {
+		this.bullets[e.id] = e;
+		e.destroy();
+	}
+	
+	
+	/**
+	 * Clears out the entities
+	 */
+	public void clear() {
+		for(int i = 0; i < this.bullets.length; i++) {
+			if(this.bullets[i] != null) {
+				this.bullets[i].destroy();
+			}						
+		}
+	}
+	
+}
---
+++ src/seventh/client/ClientEntities.java
@@ -0,0 +1,93 @@
+/*
+ * see license.txt 
+ */
+package seventh.client;
+
+/**
+ * @author Tony
+ *
+ */
+public class ClientEntities {
+
+	private ClientEntity[] entities;
+	
+	/**
+	 * @param maxEntities
+	 */
+	public ClientEntities(int maxEntities) {
+		this.entities = new ClientEntity[maxEntities];
+	}
+	
+	/**
+	 * @return the entities
+	 */
+	public ClientEntity[] getEntities() {
+		return entities;
+	}
+	
+	/**
+	 * @param id
+	 * @return the {@link ClientEntity} at the supplied position or null if not available
+	 */
+	public ClientEntity getEntity(int id) {
+		if(id > -1 && id < this.entities.length) {
+			return this.entities[id];
+		}
+		return null;
+	}
+	
+	/**
+	 * @param id
+	 * @return true if the entity exists in this list
+	 */
+	public boolean containsEntity(int id) {
+		if(id > -1 && id < this.entities.length) {
+			return this.entities[id] != null;
+		}
+		return false; 
+	}
+	
+	/**
+	 * Adds the entity
+	 * @param e
+	 */
+	public void addEntity(int id, ClientEntity e) {
+		this.entities[id] = e;
+		e.id = id;
+	}
+	
+	/**
+	 * Removes {@link ClientEntity}
+	 * @param id
+	 */
+	public ClientEntity removeEntity(int id) {	
+		ClientEntity result = null;
+		if(id > -1 && id < this.entities.length) {
+			result = this.entities[id];
+			this.entities[id] = null;
+		}
+		return result;
+	}
+	
+	/**
+	 * Removes a {@link ClientEntity}
+	 * @param e
+	 */
+	public ClientEntity removeEntity(ClientEntity e) {
+		return removeEntity(e.getId());
+	}
+	
+	/**
+	 * Clears out the entities
+	 */
+	public void clear() {
+		for(int i = 0; i < this.entities.length; i++) {
+			if(this.entities[i] != null) {
+				this.entities[i].destroy();
+			}
+			
+			this.entities[i] = null;
+		}
+	}
+	
+}
