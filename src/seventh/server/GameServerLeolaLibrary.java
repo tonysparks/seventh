48e9f606def56b62317c4f0d969fdf8b44791d89
tonysparks <anthony.sparks31@gmail.com>
2015-12-06 17:10:14 -0600

    Removed callbacks from game scripting API (still a WIP)

--- src/seventh/server/GameServerLeolaLibrary.java
+++ src/seventh/server/GameServerLeolaLibrary.java
@@ -7,22 +7,17 @@ import leola.vm.Leola;
 import leola.vm.exceptions.LeolaRuntimeException;
 import leola.vm.lib.LeolaLibrary;
 import leola.vm.types.LeoNamespace;
-import leola.vm.types.LeoObject;
 import seventh.ai.AICommand;
 import seventh.ai.basic.DefaultAISystem;
 import seventh.ai.basic.actions.Action;
 import seventh.game.Game;
 import seventh.game.PlayerInfo;
 import seventh.game.Team;
-import seventh.game.events.RoundEndedEvent;
-import seventh.game.events.RoundEndedListener;
-import seventh.game.events.RoundStartedEvent;
-import seventh.game.events.RoundStartedListener;
 import seventh.math.Vector2f;
 import seventh.shared.Cons;
 
 /**
- * An API for the maps.
+ * The server-side (game side) script API.
  * 
  * @author Tony
  *
@@ -97,31 +92,5 @@ public class GameServerLeolaLibrary implements LeolaLibrary {
 		if(info != null) {
 			system.getBrain(id).getCommunicator().makeTopPriority(action);
 		}		
-	}
-	
-	public void onStart(final LeoObject function) {
-		game.getDispatcher().addEventListener(RoundStartedEvent.class, new RoundStartedListener() {
-			
-			@Override
-			public void onRoundStarted(RoundStartedEvent event) {
-				LeoObject result = function.call();
-				if(result.isError()) {
-					Cons.println("*** ERROR: onStart errored with: " + result);
-				}
-			}
-		});
-	}
-	
-	public void onEnd(final LeoObject function) {
-		game.getDispatcher().addEventListener(RoundEndedEvent.class, new RoundEndedListener() {
-			
-			@Override
-			public void onRoundEnded(RoundEndedEvent event) {			
-				LeoObject result = function.call();
-				if(result.isError()) {
-					Cons.println("*** ERROR: onEnd errored with: " + result);
-				}
-			}
-		});
 	}
 }
