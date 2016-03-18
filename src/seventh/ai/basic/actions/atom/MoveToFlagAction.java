853e67998b862f027cc9f2c4c608cac901981b41
tonysparks <anthony.sparks31@gmail.com>
2016-03-17 22:04:19 -0500

    added zone avoidance

--- src/seventh/ai/basic/actions/atom/MoveToFlagAction.java
+++ src/seventh/ai/basic/actions/atom/MoveToFlagAction.java
@@ -3,18 +3,28 @@
  */
 package seventh.ai.basic.actions.atom;
 
+import java.util.Collections;
+import java.util.List;
+
+import seventh.ai.basic.Zone;
 import seventh.game.Flag;
 
 /**
  * @author Tony
  *
  */
-public class MoveToFlagAction extends MoveToAction {
+public class MoveToFlagAction extends AvoidMoveToAction {
+	
+	private static final List<Zone> emptyZonesToAvoid = Collections.emptyList();
 	
 	/**
 	 * @param target 
 	 */
-	public MoveToFlagAction(Flag target) {
-		super(target.getCenterPos());		
+	public MoveToFlagAction(Flag target, List<Zone> zonesToAvoid) {
+		super(target.getCenterPos(), zonesToAvoid);		
 	}	
+	
+	public MoveToFlagAction(Flag target) {
+		super(target.getCenterPos(), emptyZonesToAvoid);		
+	}
 }
