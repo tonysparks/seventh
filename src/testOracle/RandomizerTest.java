6e86fecd01d9be7bd426f1b1fe8679e218d84e59
skagh <skagh@NamHoLaptop>
2017-03-30 11:18:09 +0900

    Purpose: specify a minimum scope Input: getRandomRangeMin minimum scope 0.5 Expected: 		0.5 < random number 		random number < 1.0

--- src/testOracle/RandomizerTest.java
+++ src/testOracle/RandomizerTest.java
@@ -2,41 +2,27 @@ package testOracle;
 
 import static org.junit.Assert.*;
 import java.util.Random;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import seventh.shared.Randomizer;
 
 public class RandomizerTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	/*
+	 * Purpose: specify a minimum scope
+	 * Input: getRandomRangeMin minimum scope 0.5
+	 * Expected: 
+	 * 			0.5 < random number
+	 * 			random number < 1.0
+	 */
 	@Test
-	public void test() {
-		Random random = new Random();
-		Randomizer randomizer = new Randomizer(random);
-		randomizer.getRandomRangeMin(0.5);
-		
-		//randomizer.nextFloat();
-		assertTrue(0.5 < randomizer.getRandomRange(random, 2.0,1.0));
+	public void testMinScope() {
+		final double rangeMin = 0.5;
+		final double rangeMax = 1.0;
+		Randomizer randomizer = new Randomizer(new Random());
+		assertTrue(rangeMin < randomizer.getRandomRangeMin(rangeMin));
+		assertTrue(rangeMax > randomizer.getRandomRangeMin(rangeMin));
 	}
+
 
 }
