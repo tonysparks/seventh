0d93105ca567a7a5fe1a985818f436812a7dc4d5
Daniel Kim <skaghzz@naver.com>
2017-04-02 00:31:13 +0900

    testChangeCurrentStateEnter
    Purpose: change the current state to enter with no Listener
    Input: test state => ServerStartState
    Expected: 
    			currentState is enter
    	 * 			listener is NULL

--- src/testOracle/StateMachineTest.java
+++ src/testOracle/StateMachineTest.java
@@ -4,7 +4,13 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import seventh.server.GameServer;
+import seventh.server.InGameState;
+import seventh.server.ServerStartState;
+import seventh.shared.State;
 import seventh.shared.StateMachine;
+import seventh.shared.StateMachine.StateMachineListener;
+import seventh.shared.TimeStep;
 
 public class StateMachineTest {
 
@@ -18,4 +24,21 @@ public class StateMachineTest {
 	public void testCurrentStateAfterConstruct() {
 		assertNull(new StateMachine<>().getCurrentState());
 	}
+	
+	/*
+	 * Purpose: change the current state to enter with no Listener
+	 * Input: test state => ServerStartState
+	 * Expected: 
+	 * 			currentState is enter
+	 * 			listener is NULL
+	 */
+	@Test
+	public void testChangeCurrentStateEnter(){
+		StateMachine<State> statemachine = new StateMachine<State>();
+		ServerStartState expectedState = new ServerStartState();
+		statemachine.changeState(expectedState);
+		assertEquals(expectedState,statemachine.getCurrentState());
+		assertNull(statemachine.getListener());
+	}
+	
 }
