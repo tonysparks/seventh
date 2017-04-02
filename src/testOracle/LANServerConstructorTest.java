2fe57548b57da198d5240137327148635d08fddc
HyeongGun Lee <gudrjs012@gmail.com>
2017-04-02 21:34:52 +0900

    Test LANServerConstructor

---
+++ src/testOracle/LANServerConstructorTest.java
@@ -0,0 +1,24 @@
+package testOracle;
+
+import static org.junit.Assert.*;
+
+import org.easymock.EasyMock;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import seventh.shared.Config;
+import seventh.shared.LANServerConfig;
+
+public class LANServerConstructorTest {
+
+	private Config config;
+	private LANServerConfig lanconfig;
+	
+	@Test
+	public void testLANServerConfig(){
+		lanconfig = new LANServerConfig(config);
+	}
+	
+
+}
