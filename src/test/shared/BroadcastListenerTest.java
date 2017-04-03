adcc3f0f478339f5b6bb3dd3fa0156c01285423f
runningo <erunningo@gmail.com>
2017-04-03 23:21:35 +0900

    test AddOnMessageReceivedListener function in Broadcaster class.
    test AddOnMessageReceivedListener function in Broadcaster class.

--- src/test/shared/BroadcastListenerTest.java
+++ src/test/shared/BroadcastListenerTest.java
@@ -18,16 +18,16 @@ public class BroadcastListenerTest {
 		BroadcastListener test = new BroadcastListener(20,"192.168.25.53",80);
 	}
 
-//	/*
-//	 * purpose : test AddOnMessageReceivedListener function in Broadcaster class.
-//	 * input : mtu:20, groupAddress:192.168.25.53, port:80, null for OnMessageREceivedListener
-//	 * expected output : X, cause the function is only made for creation.
-//	 */
-//	@Test
-//	public void testAddOnMessageReceivedListener() {
-//		BroadcastListener test = new BroadcastListener(20,"192.168.25.53",80);
-//		test.addOnMessageReceivedListener(null);
-//	}
+	/*
+	 * purpose : test AddOnMessageReceivedListener function in Broadcaster class.
+	 * input : mtu:20, groupAddress:192.168.25.53, port:80, null for OnMessageREceivedListener
+	 * expected output : X, cause the function is only made for creation.
+	 */
+	@Test
+	public void testAddOnMessageReceivedListener() {
+		BroadcastListener test = new BroadcastListener(20,"192.168.25.53",80);
+		test.addOnMessageReceivedListener(null);
+	}
 
 //	@Test
 //	public void testStart() {
