ea4900813c32ff9211713c5e221eb5d458f7c810
runningo <erunningo@gmail.com>
2017-04-03 23:10:10 +0900

    test Broadcaster function in Broadcaster class.
    test Broadcaster function in Broadcaster class.

---
+++ src/test/shared/BroadcasterTest.java
@@ -0,0 +1,47 @@
+package test.shared;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import seventh.shared.Broadcaster;
+
+public class BroadcasterTest {
+
+	/*
+	 * purpose : test Broadcaster function in Broadcaster class.
+	 * input : mtu:20, groupAddress:192.168.25.53, port:80
+	 * expected output : X, cause the function is only made for creation.
+	 */
+	@Test
+	public void testBroadcaster() {
+		try {
+			Broadcaster test = new Broadcaster(20,"192.168.25.53",80);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		
+	}
+
+//	@Test
+//	public void testBroadcastMessage() {
+//		try {
+//			Broadcaster test = new Broadcaster(5,"192.168.25.53",80);			
+//			test.broadcastMessage("Hello");
+//			test.broadcastMessage("HelloHello");
+//			
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			
+//		}
+//		
+//	}
+
+//	@Test
+//	public void testClose() {
+//		fail("Not yet implemented");
+//	}
+
+}
