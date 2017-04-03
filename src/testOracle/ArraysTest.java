f4d62d6cfc5b8862c4a32424408e49748aa7b8da
runningo <erunningo@gmail.com>
2017-04-03 17:04:02 +0900

    test clear function in Arrays class.
    test clear function in Arrays class.

--- src/testOracle/ArraysTest.java
+++ src/testOracle/ArraysTest.java
@@ -18,13 +18,14 @@ public class ArraysTest {
 	 * 					assertEquals 1 for incaseStuff,
 	 * 					assertEquals 0 for nullStuff.
 	 */
+	@SuppressWarnings("unchecked")
 	@Test
 	public <T> void testUsedLength() {
 		
-		@SuppressWarnings("unchecked")
 		T[] zeroStuff = (T[])new Object[3];
 		T[] incaseStuff = (T[])new Object[1];
 		T[] nullStuff = null;
+		assertNull(nullStuff);
 		incaseStuff[0] = (T) "A";
 		assertEquals(Arrays.usedLength(zeroStuff),0);
 		assertEquals(Arrays.usedLength(incaseStuff),1);
@@ -33,9 +34,29 @@ public class ArraysTest {
 		
 	}
 
-//	@Test
-//	public void testClear() {
-//		fail("Not yet implemented");
-//	}
+	/*
+	 * purpose : test clear function in Arrays class.
+	 * input : generic type [T] array. 
+	 * 			incaseStuff for "if sentence not null",
+	 * 			nullStuff for "if sentence null" 
+	 * output : assertNull for incaseStuff is true.
+	 * 			***but in this case, an error about JavaDoc is poped up.**
+	 */
+	@SuppressWarnings("unchecked")
+	@Test
+	public <T> void testClear() {
+		
+		T[] incaseStuff = (T[])new Object[1];
+		incaseStuff[0] = (T) "A";
+		assertNotNull(incaseStuff);
+		
+		T[] nullStuff = null;
+		assertNull(nullStuff);
+		
+		Arrays.clear(incaseStuff);		
+		Arrays.clear(nullStuff);
+//		assertNull(incaseStuff);
+		
+	}
 
 }
