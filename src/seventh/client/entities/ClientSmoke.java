edadcd12df34e785ef5ef1c8b599008dc873ef20
tonysparks <anthony.sparks31@gmail.com>
2017-04-05 22:45:10 -0500

    made smoke emitter

--- src/seventh/client/entities/ClientSmoke.java
+++ src/seventh/client/entities/ClientSmoke.java
@@ -4,15 +4,9 @@
 package seventh.client.entities;
 
 import seventh.client.ClientGame;
-import seventh.client.gfx.AnimatedImage;
-import seventh.client.gfx.Art;
 import seventh.client.gfx.Camera;
 import seventh.client.gfx.Canvas;
-import seventh.client.gfx.effects.particle_system.Emitter;
 import seventh.client.gfx.effects.particle_system.Emitters;
-import seventh.client.sfx.Sounds;
-import seventh.game.net.NetEntity;
-import seventh.game.net.NetSmoke;
 import seventh.math.Vector2f;
 import seventh.shared.TimeStep;
 
@@ -21,55 +15,27 @@ import seventh.shared.TimeStep;
  *
  */
 public class ClientSmoke extends ClientEntity {
-    
-    private boolean soundPlayed;    
-    private long ownerId;    
-    private Emitter smoke;    
-    private AnimatedImage fireImg;
+        
     /**
      * 
      */
     public ClientSmoke(ClientGame game, Vector2f pos) {
         super(game, pos);
         
-        this.soundPlayed = false;
-        
         bounds.width = 16;
         bounds.height = 16;
-        //smoke = Emitters.newFireEmitter(pos);//new FireEmitter(pos, 3_900, 0, 500);
-        //smoke.attachTo(this);
-        
-        fireImg = Art.newFireAnim();
+        game.addForegroundEffect(Emitters.newSmokeEmitter(pos, 18_000, 0x238261ff, 0x838B83ff, 40, .9f, 1.6f)
+                                         .attachTo(this)
+                                         .setDieInstantly(false));        
     }
-        
-    /* (non-Javadoc)
-     * @see palisma.client.ClientEntity#update(leola.live.TimeStep)
-     */
+       
     @Override
     public void update(TimeStep timeStep) {    
-        super.update(timeStep);
-        
-//        if(!soundPlayed) {
-//            Sounds.playSound(Sounds.explodeSnd, ownerId, pos);
-//            this.soundPlayed = true;
-//        }        
-//        
-//        smoke.update(timeStep);
-//        fireImg.update(timeStep);
+        super.update(timeStep);        
     }
 
-    /* (non-Javadoc)
-     * @see leola.live.gfx.Renderable#render(leola.live.gfx.Canvas, leola.live.gfx.Camera, long)
-     */
     @Override
     public void render(Canvas canvas, Camera camera, float alpha) {        
-        
-//        Vector2f cameraPos = camera.getPosition();
-//        int x = (int)(pos.x - cameraPos.x);
-//        int y = (int)(pos.y - cameraPos.y);
-        
-        //canvas.drawScaledImage(fireImg.getCurrentImage(), x-bounds.width/4, y-bounds.height/4, 32, 32, 0x3fffffff);
-//        smoke.render(canvas, camera, alpha);
     }
 
 }
