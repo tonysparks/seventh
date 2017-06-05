a305ba933fedd88ecc8efc7f8ef9b943c855471a
Daniel Kim <skaghzz@naver.com>
2017-06-05 20:50:05 +0900

    return to original code

--- src/seventh/game/type/ObjectiveGameType.java
+++ src/seventh/game/type/ObjectiveGameType.java
@@ -24,227 +24,216 @@ import seventh.shared.TimeStep;
  *
  */
 public class ObjectiveGameType extends AbstractTeamGameType {
+    
+    private int currentRound;
+    private int minimumObjectivesToComplete;
+    private List<Objective> outstandingObjectives;
+    private List<Objective> completedObjectives;
+    private long roundDelayTime;
+    private long currentDelayTime;
+    private boolean inIntermission;
+    
+    private Team attacker, defender;
+    
+    /**
+     * @param maxScore
+     * @param roundTime
+     */
+    public ObjectiveGameType(Leola runtime,
+                             List<Objective> objectives,     
+                             List<Vector2f> alliedSpawnPoints,
+                             List<Vector2f> axisSpawnPoints,
+                             int minimumObjectivesToComplete,
+                             int maxScore, 
+                             long roundTime, 
+                             long roundDelayTime,
+                             byte defenderTeamId) {
+        
+        super(GameType.Type.OBJ, runtime, alliedSpawnPoints, axisSpawnPoints, maxScore, roundTime);
+        
+        this.outstandingObjectives = objectives;        
+        this.minimumObjectivesToComplete = minimumObjectivesToComplete;        
+        this.currentRound = 0;
+        this.roundDelayTime = roundDelayTime;
+        
+        this.currentDelayTime = roundDelayTime;        
+        this.inIntermission = true;    
+        
+        this.completedObjectives = new ArrayList<Objective>(this.outstandingObjectives.size());
+        
+        if(defenderTeamId == Team.ALLIED_TEAM_ID) {
+            this.attacker = getAxisTeam();
+            this.defender = getAlliedTeam();            
+        }
+        else {
+            this.defender = getAxisTeam();
+            this.attacker = getAlliedTeam();                        
+        }
+        
+        this.attacker.setAttacker(true);
+        this.defender.setDefender(true);
+    }
+    
+    /**
+     * @return the attacker
+     */
+    public Team getAttacker() {
+        return attacker;
+    }
+    
+    /**
+     * @return the defender
+     */
+    public Team getDefender() {
+        return defender;
+    }
+    
+    
+    /* (non-Javadoc)
+     * @see seventh.game.type.GameType#start(seventh.game.Game)
+     */
+    @Override
+    public void start(Game game) {
+        setGameState(GameState.IN_PROGRESS);
+        int size = this.outstandingObjectives.size();
+        for(int i = 0; i < size; i++) {
+            this.outstandingObjectives.get(i).init(game);
+        }
+    }
 
-	private int currentRound;
-	private int minimumObjectivesToComplete;
-	private List<Objective> outstandingObjectives;
-	private List<Objective> completedObjectives;
-	private long roundDelayTime;
-	private long currentDelayTime;
-	private boolean inIntermission;
+    /* (non-Javadoc)
+     * @see seventh.game.type.GameType#update(leola.live.TimeStep)
+     */
+    @Override
+    protected GameState doUpdate(Game game, TimeStep timeStep) {
+        
+        if(this.inIntermission) {
+            this.currentDelayTime -= timeStep.getDeltaTime();
+            
+            if(this.currentDelayTime <= 0) {
+                startRound(game);
+            }
+        }
+        else {
 
-	private Team attacker, defender;
+            // If there are objectives that are in progress
+            // we must force the attackers to disarm them,
+            // even if all the attackers are dead
+            int numberOfObjectivesInProgress = 0;
+            
+            // if we are currently playing, check
+            // and see if the objectives have been completed        
+            int size = this.outstandingObjectives.size();
+            for(int i = 0; i < size; i++) {
+                Objective obj = this.outstandingObjectives.get(i); 
+                if (obj.isCompleted(game)) {
+                    this.completedObjectives.add(obj);
+                }
+                else if(obj.isInProgress(game)) {
+                    numberOfObjectivesInProgress++;
+                }
+            }
+            
+            this.outstandingObjectives.removeAll(this.completedObjectives);
+            
+            
+            if(this.completedObjectives.size() >= this.minimumObjectivesToComplete) {
+                endRound(attacker, game);
+            }
+            else if(this.outstandingObjectives.isEmpty() && this.completedObjectives.size() > 0) {
+                endRound(attacker, game);
+            }
+            else if( defender.isTeamDead() && defender.teamSize() > 0) {
+                endRound(attacker, game);
+            }    
+            else if(getRemainingTime() <= 0 ) {
+                endRound(defender, game);
+            }
+            else if( (attacker.isTeamDead() && attacker.teamSize() > 0)
+                && (numberOfObjectivesInProgress+this.completedObjectives.size() < this.minimumObjectivesToComplete) ) {
+                //&& (!this.outstandingObjectives.isEmpty() ? numberOfObjectivesInProgress < this.outstandingObjectives.size() : true) ) {
+                endRound(defender, game);
+            }            
+            else {
+                checkSpectating(timeStep, game);
+            }                
+        }
+        
+        return this.currentRound >= this.getMaxScore() ? GameState.WINNER : GameState.IN_PROGRESS;
+        
+    }
+    
+    /* (non-Javadoc)
+     * @see seventh.game.type.AbstractTeamGameType#isInProgress()
+     */
+    @Override
+    public boolean isInProgress() {    
+        return super.isInProgress() && (!this.inIntermission || this.currentDelayTime == this.roundDelayTime);
+    }
+        
+    /**
+     * End the round
+     * 
+     * @param winner
+     * @param game
+     */
+    private void endRound(Team winner, Game game) {
+        this.currentRound++;
+        this.currentDelayTime = this.roundDelayTime;
+        this.inIntermission = true;        
+        
+        // give a point to the winner
+        if(winner!=null) {
+            winner.score(1);
+        }
+        
+        getDispatcher().queueEvent(new RoundEndedEvent(this, winner, game.getNetGameStats()));            
+        
+    }
+    
+    private void startRound(Game game) {        
+        this.inIntermission = false;
 
-	/**
-	 * @param maxScore
-	 * @param roundTime
-	 */
-	public ObjectiveGameType(Leola runtime, List<Objective> objectives, List<Vector2f> alliedSpawnPoints,
-			List<Vector2f> axisSpawnPoints, int minimumObjectivesToComplete, int maxScore, long roundTime,
-			long roundDelayTime, byte defenderTeamId) {
+        game.killAll();
+        
+        int size = this.completedObjectives.size();
+        for(int i = 0; i < size; i++) {
+            this.completedObjectives.get(i).reset(game);
+        }    
+        
+        size = this.outstandingObjectives.size();
+        for(int i = 0; i < size; i++) {
+            outstandingObjectives.get(i).reset(game);
+        }
+                
+        this.outstandingObjectives.addAll(completedObjectives);
+        this.completedObjectives.clear();
+        
+        
+        // if we are done with intermission, lets go 
+        // ahead and spawn the players
+        Player[] players = game.getPlayers().getPlayers();
+        for (int i = 0; i < players.length; i++) {
+            Player player = players[i];
+            if (player != null) {
+                if(!player.isPureSpectator() && !player.isCommander()) {
+                    spawnPlayer(player, game);
+                }
+            }
+        }
+        
+        resetRemainingTime();
+                                    
+        getDispatcher().queueEvent(new RoundStartedEvent(this));
+    }
 
-		super(GameType.Type.OBJ, runtime, alliedSpawnPoints, axisSpawnPoints, maxScore, roundTime);
 
-		this.outstandingObjectives = objectives;
-		this.minimumObjectivesToComplete = minimumObjectivesToComplete;
-		this.currentRound = 0;
-		this.roundDelayTime = roundDelayTime;
+    /* (non-Javadoc)
+     * @see seventh.game.type.GameType#registerListeners(seventh.game.Game, leola.frontend.listener.EventDispatcher)
+     */
+    @Override
+    protected void doRegisterListeners(Game game, EventDispatcher dispatcher) {        
+    }
 
-		this.currentDelayTime = roundDelayTime;
-		this.inIntermission = true;
-
-		this.completedObjectives = new ArrayList<Objective>(this.outstandingObjectives.size());
-
-		if (defenderTeamId == Team.ALLIED_TEAM_ID) {
-			this.attacker = getAxisTeam();
-			this.defender = getAlliedTeam();
-		} else {
-			this.defender = getAxisTeam();
-			this.attacker = getAlliedTeam();
-		}
-
-		this.attacker.setAttacker(true);
-		this.defender.setDefender(true);
-	}
-
-	/**
-	 * @return the attacker
-	 */
-	public Team getAttacker() {
-		return attacker;
-	}
-
-	/**
-	 * @return the defender
-	 */
-	public Team getDefender() {
-		return defender;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see seventh.game.type.GameType#start(seventh.game.Game)
-	 */
-	@Override
-	public void start(Game game) {
-		setGameState(GameState.IN_PROGRESS);
-		int size = this.outstandingObjectives.size();
-		for (int i = 0; i < size; i++) {
-			this.outstandingObjectives.get(i).init(game);
-		}
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see seventh.game.type.GameType#update(leola.live.TimeStep)
-	 */
-	@Override
-	protected GameState doUpdate(Game game, TimeStep timeStep) {
-		/*
-		 * Refactoring target : if and else if statements Refactoring name :
-		 * Introduce explaining variable Bad smell(reason) : Put the result of
-		 * the expression, or parts of the expression, in a temporary variable
-		 * with a name that explains the purpose
-		 * 
-		 */
-		// If there are objectives that are in progress
-		// we must force the attackers to disarm them,
-		// even if all the attackers are dead
-		int numberOfObjectivesInProgress = 0;
-		boolean overMinimumSize = this.completedObjectives.size() >= this.minimumObjectivesToComplete;
-		boolean isEmpty = this.outstandingObjectives.isEmpty() && this.completedObjectives.size() > 0;
-		boolean defenderDead = defender.isTeamDead() && defender.teamSize() > 0;
-		boolean getTime = getRemainingTime() <= 0;
-		boolean attackerDead = (attacker.isTeamDead() && attacker.teamSize() > 0);
-		boolean checkMin = numberOfObjectivesInProgress
-				+ this.completedObjectives.size() < this.minimumObjectivesToComplete;
-
-		boolean intermission = this.inIntermission;
-		/*
-		 * Refactoring target : if else statements Refactoring name : erase
-		 * nesting if statement, extract function Bad smell(reason) : nesting if
-		 * statement
-		 * 
-		 */
-		if (!intermission) {
-			// if we are currently playing, check
-			// and see if the objectives have been completed
-			calculateObj(game, numberOfObjectivesInProgress);
-
-			this.outstandingObjectives.removeAll(this.completedObjectives);
-			/*
-			 * Refactoring target : if and else if statements Refactoring name :
-			 * erase duplicated statement Bad smell(reason) : needless
-			 * duplicated statement
-			 */
-			if (overMinimumSize || isEmpty || defenderDead)
-				endRound(attacker, game);
-			else if (getTime || (attackerDead && checkMin))
-				endRound(defender, game);
-			else
-				checkSpectating(timeStep, game);
-			return this.currentRound >= this.getMaxScore() ? GameState.WINNER : GameState.IN_PROGRESS;
-		}
-
-		this.currentDelayTime -= timeStep.getDeltaTime();
-
-		if (this.currentDelayTime <= 0) {
-			startRound(game);
-		}
-
-		return this.currentRound >= this.getMaxScore() ? GameState.WINNER : GameState.IN_PROGRESS;
-
-	}
-
-	private void calculateObj(Game game, int numberOfObjectivesInProgress) {
-		int size = this.outstandingObjectives.size();
-		for (int i = 0; i < size; i++) {
-			Objective obj = this.outstandingObjectives.get(i);
-			if (obj.isCompleted(game)) {
-				this.completedObjectives.add(obj);
-			} else if (obj.isInProgress(game)) {
-				numberOfObjectivesInProgress++;
-			}
-		}
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see seventh.game.type.AbstractTeamGameType#isInProgress()
-	 */
-	@Override
-	public boolean isInProgress() {
-		return super.isInProgress() && (!this.inIntermission || this.currentDelayTime == this.roundDelayTime);
-	}
-
-	/**
-	 * End the round
-	 * 
-	 * @param winner
-	 * @param game
-	 */
-	private void endRound(Team winner, Game game) {
-		this.currentRound++;
-		this.currentDelayTime = this.roundDelayTime;
-		this.inIntermission = true;
-
-		// give a point to the winner
-		if (winner != null) {
-			winner.score(1);
-		}
-
-		getDispatcher().queueEvent(new RoundEndedEvent(this, winner, game.getNetGameStats()));
-
-	}
-
-	private void startRound(Game game) {
-		this.inIntermission = false;
-
-		game.killAll();
-
-		int size = this.completedObjectives.size();
-		for (int i = 0; i < size; i++) {
-			this.completedObjectives.get(i).reset(game);
-		}
-
-		size = this.outstandingObjectives.size();
-		for (int i = 0; i < size; i++) {
-			outstandingObjectives.get(i).reset(game);
-		}
-
-		this.outstandingObjectives.addAll(completedObjectives);
-		this.completedObjectives.clear();
-
-		// if we are done with intermission, lets go
-		// ahead and spawn the players
-		Player[] players = game.getPlayers().getPlayers();
-		for (int i = 0; i < players.length; i++) {
-			Player player = players[i];
-			/*
-			 * Refactoring target : nested if statements Refactoring name :
-			 * erase needless if statement Bad smell(reason) : nested if
-			 * statements
-			 */
-			if (player != null && (!player.isPureSpectator() && !player.isCommander()))
-				spawnPlayer(player, game);
-		}
-
-		resetRemainingTime();
-
-		getDispatcher().queueEvent(new RoundStartedEvent(this));
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see seventh.game.type.GameType#registerListeners(seventh.game.Game,
-	 * leola.frontend.listener.EventDispatcher)
-	 */
-	@Override
-	protected void doRegisterListeners(Game game, EventDispatcher dispatcher) {
-	}
-
+    
 }
--- src/seventh/game/type/ObjectiveScript.java
+++ src/seventh/game/type/ObjectiveScript.java
@@ -7,8 +7,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Assert;
-
 import leola.vm.Leola;
 import leola.vm.types.LeoArray;
 import leola.vm.types.LeoNativeClass;
@@ -25,180 +23,105 @@ import seventh.shared.Cons;
  */
 public class ObjectiveScript extends AbstractGameTypeScript {
 
-	/**
-	 * 
-	 */
-	public ObjectiveScript(Leola runtime) {
-		super(runtime);
-	}
+    /**
+     * 
+     */
+    public ObjectiveScript(Leola runtime) {
+        super(runtime);
+    }
 
-	/**
-	 * @param mapFile
-	 * @param maxScore
-	 * @param matchTime
-	 * @return
-	 * @throws Exception
-	 */
-	public GameType loadGameType(String mapFile, int maxScore, long matchTime) throws Exception {
-		List<Objective> objectives = new ArrayList<>();
-		List<Vector2f> alliedSpawnPoints = new ArrayList<>();
-		List<Vector2f> axisSpawnPoints = new ArrayList<Vector2f>();
-		byte defenders = Team.AXIS_TEAM_ID;
-		int minimumObjectivesToComplete = 1;
-
-		File scriptFile = new File(mapFile + ".obj.leola");
-		/*
-		 * Refactoring target : scriptFile.exist() Refactoring name : Introduce
-		 * Assertion Bad smell(reason) : A section of code assumes something
-		 * about the state of the program
-		 * 
-		 */
-		Assert.assertTrue(scriptFile != null);
-		/*
-		 * Refactoring target : scriptFile.exist() Refactoring name : Replace
-		 * Error Code with exception, Erase nested if statement Bad
-		 * smell(reason) : "If statement" returns a special code to indicate an
-		 * error and check only file exist
-		 * 
-		 */
-		fileExist(scriptFile);
-
-		/*
-		 * Refactoring target : LeoObject.isTrue(config),
-		 * LeoObject.isTrue(scriptedobjectives) Refactoring name : Replace Error
-		 * Code with exception, Erase nested if statement Bad smell(reason) :
-		 * "If statement" check only instance of LeoObject existed or not
-		 */
-		LeoObject config = getRuntime().eval(scriptFile);
-		checkLeoObject(config);
-		LeoObject scriptedObjectives = config.getObject("objectives");
-		checkLeoObject(scriptedObjectives);
-
-		/*
-		 * Refactoring target : from the statement
-		 * "if (o.getValue() instanceof Objective)" to statement "else"
-		 * Refactoring name : extract function Bad smell(reason) : duplicated
-		 * statement in switch sentence
-		 */
-
-		/*
-		 * Refactoring target : switch statements about getType Refactoring name
-		 * : extract function Bad smell(reason) : code complexity
-		 */
-
-		checkObjectivesType(objectives, scriptedObjectives);
-		LeoObject scriptedDefenders = config.getObject("defenders");
-		checkLeoObject(scriptedDefenders);
-		defenders = checkDefendersType(defenders, scriptedDefenders);
-
-		alliedSpawnPoints = loadSpawnPoint(config, "alliedSpawnPoints");
-		axisSpawnPoints = loadSpawnPoint(config, "axisSpawnPoints");
-		minimumObjectivesToComplete = setMinObjToCom(objectives, config);
-
-		final long timeBetweenRounds = 10_000L;
-
-		GameType gameType = new ObjectiveGameType(getRuntime(), objectives, alliedSpawnPoints, axisSpawnPoints,
-				minimumObjectivesToComplete, maxScore, matchTime, timeBetweenRounds, defenders);
-		/*
-		 * Refactoring target : return gameType; Refactoring name : Introduce
-		 * Assertion Bad smell(reason) : A section of code assumes something
-		 * about the state of the program
-		 * 
-		 */
-		Assert.assertTrue(gameType != null);
-		return gameType;
-	}
-
-	private byte checkDefendersType(byte defenders, LeoObject scriptedDefenders) {
-		switch (scriptedDefenders.getType()) {
-		case INTEGER:
-		case LONG:
-		case REAL:
-			defenders = (byte) scriptedDefenders.asInt();
-			break;
-		case STRING:
-			if (Team.ALLIED_TEAM_NAME.equalsIgnoreCase(scriptedDefenders.toString())) {
-				defenders = Team.ALLIED_TEAM_ID;
-			}
-			break;
-		default: {
-			Cons.println(
-					"*** ERROR -> defenders must either be a 2(for allies) or 4(for axis) or 'allies' or 'axis' values");
-		}
-		}
-		return defenders;
-	}
-
-	private void checkObjectivesType(List<Objective> objectives, LeoObject scriptedObjectives) {
-		switch (scriptedObjectives.getType()) {
-		case ARRAY: {
-			LeoArray array = scriptedObjectives.as();
-			for (int i = 0; i < array.size(); i++) {
-				LeoObject o = array.get(i);
-				if (o instanceof LeoNativeClass) {
-					setObjective(objectives, o);
-				}
-			}
-			break;
-		}
-		case NATIVE_CLASS: {
-			LeoObject o = scriptedObjectives;
-			setObjective(objectives, o);
-			break;
-		}
-		default: {
-			Cons.println(
-					"*** ERROR -> objectives must either be an Array of objectives or a Java class or custom Leola class");
-		}
-		}
-	}
-
-	private void setObjective(List<Objective> objectives, LeoObject o) {
-		if (o.getValue() instanceof Objective) {
-			Objective objective = (Objective) o.getValue();
-			objectives.add(objective);
-		} else {
-			Cons.println(((LeoNativeClass) o).getNativeClass() + " is not of type: " + Objective.class.getName());
-		}
-	}
-
-	/*
-	 * Refactoring target : minimumObjectivesToComplete Refactoring name :
-	 * extract function Bad smell(reason) : Turn the statements into its own
-	 * function
-	 * 
-	 */
-	private int setMinObjToCom(List<Objective> objectives, LeoObject config) {
-		int minimumObjectivesToComplete;
-		if (config.hasObject("minimumObjectivesToComplete")) {
-			minimumObjectivesToComplete = config.getObject("minimumObjectivesToComplete").asInt();
-		} else {
-			minimumObjectivesToComplete = objectives.size();
-		}
-		return minimumObjectivesToComplete;
-	}
-
-	private void fileExist(File scriptFile) {
-		try {
-			if (!scriptFile.exists()) {
-				throw new Exception();
-			}
-		} catch (Exception e) {
-			Cons.println("*** ERROR -> No associated script file for objective game type.  Looking for: "
-					+ scriptFile.getName());
-		}
-
-	}
-
-	private void checkLeoObject(LeoObject Leo) {
-		try {
-			if (!LeoObject.isTrue(Leo)) {
-				throw new Exception();
-			}
-		} catch (Exception e) {
-			Cons.println("ERROR");
-		}
-
-	}
-
+    /**
+     * @param mapFile
+     * @param maxScore
+     * @param matchTime
+     * @return
+     * @throws Exception
+     */
+    public GameType loadGameType(String mapFile, int maxScore, long matchTime) throws Exception {
+        List<Objective> objectives = new ArrayList<>();
+        List<Vector2f> alliedSpawnPoints = new ArrayList<>();
+        List<Vector2f> axisSpawnPoints = new ArrayList<Vector2f>();
+        byte defenders = Team.AXIS_TEAM_ID;
+        int minimumObjectivesToComplete = 1;
+        
+        File scriptFile = new File(mapFile + ".obj.leola");
+        if(!scriptFile.exists()) {
+            Cons.println("*** ERROR -> No associated script file for objective game type.  Looking for: " + scriptFile.getName());
+        }
+        else {
+            LeoObject config = getRuntime().eval(scriptFile);
+            if(LeoObject.isTrue(config)) {
+                LeoObject scriptedObjectives = config.getObject("objectives");                                
+                if(LeoObject.isTrue(scriptedObjectives)) {
+                    switch(scriptedObjectives.getType()) {
+                        case ARRAY: {
+                            LeoArray array = scriptedObjectives.as();
+                            for(int i = 0; i < array.size(); i++) {
+                                LeoObject o = array.get(i);
+                                if (o instanceof LeoNativeClass) {
+                                    if(o.getValue() instanceof Objective) {
+                                        Objective objective = (Objective)o.getValue();
+                                        objectives.add(objective);
+                                    }
+                                    else {
+                                        Cons.println(((LeoNativeClass) o).getNativeClass() + " is not of type: " + Objective.class.getName());
+                                    }
+                                    
+                                }
+                            }
+                            break;
+                        }
+                        case NATIVE_CLASS: {
+                            LeoObject o = scriptedObjectives;
+                            if(o.getValue() instanceof Objective) {
+                                Objective objective = (Objective)o.getValue();
+                                objectives.add(objective);
+                            }
+                            else {
+                                Cons.println(((LeoNativeClass) o).getNativeClass() + " is not of type: " + Objective.class.getName());
+                            }
+                            break;
+                        }
+                        default: {
+                            Cons.println("*** ERROR -> objectives must either be an Array of objectives or a Java class or custom Leola class");
+                        }
+                    }
+                }
+                
+                LeoObject scriptedDefenders = config.getObject("defenders");
+                if(LeoObject.isTrue(scriptedDefenders)) {
+                    switch(scriptedDefenders.getType()) {
+                        case INTEGER:
+                        case LONG:
+                        case REAL:
+                            defenders = (byte)scriptedDefenders.asInt();
+                            break;
+                        case STRING:
+                            if(Team.ALLIED_TEAM_NAME.equalsIgnoreCase(scriptedDefenders.toString())) {
+                                defenders = Team.ALLIED_TEAM_ID;
+                            }                            
+                            break;
+                        default:{
+                            Cons.println("*** ERROR -> defenders must either be a 2(for allies) or 4(for axis) or 'allies' or 'axis' values");
+                        }
+                    }
+                }
+                
+                alliedSpawnPoints = loadSpawnPoint(config, "alliedSpawnPoints");
+                axisSpawnPoints = loadSpawnPoint(config, "axisSpawnPoints");
+                if(config.hasObject("minimumObjectivesToComplete")) {
+                    minimumObjectivesToComplete = config.getObject("minimumObjectivesToComplete").asInt();
+                }
+                else {
+                    minimumObjectivesToComplete = objectives.size();
+                }
+            }
+        }
+        
+        final long timeBetweenRounds = 10_000L;
+        
+        GameType gameType = new ObjectiveGameType(getRuntime(), objectives, alliedSpawnPoints, axisSpawnPoints, 
+                minimumObjectivesToComplete, maxScore, matchTime, timeBetweenRounds, defenders);
+        return gameType;
+    }
 }
