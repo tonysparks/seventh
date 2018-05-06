82e78a5c78050b8137a6b8b353a4344e0195925e
GardenHee <sh8978@naver.com>
2018-05-06 18:55:34 +0900

    Refactoring Seventh.game.type.obj.ObjectiveScript.java

--- src/seventh/game/type/obj/ObjectiveScript.java
+++ src/seventh/game/type/obj/ObjectiveScript.java
@@ -45,85 +45,119 @@ public class ObjectiveScript extends AbstractGameTypeScript {
         List<Vector2f> axisSpawnPoints = new ArrayList<Vector2f>();
         byte defenders = Team.AXIS_TEAM_ID;
         int minimumObjectivesToComplete = 1;
-        
-        File scriptFile = new File(mapFile + ".obj.leola");
-        if(!scriptFile.exists()) {
-            Cons.println("*** ERROR -> No associated script file for objective game type.  Looking for: " + scriptFile.getName());
-        }
-        else {
-            LeoObject config = getRuntime().eval(scriptFile);
-            if(LeoObject.isTrue(config)) {
-                LeoObject scriptedObjectives = config.getObject("objectives");                                
-                if(LeoObject.isTrue(scriptedObjectives)) {
-                    switch(scriptedObjectives.getType()) {
-                        case ARRAY: {
-                            LeoArray array = scriptedObjectives.as();
-                            for(int i = 0; i < array.size(); i++) {
-                                LeoObject o = array.get(i);
-                                if (o instanceof LeoNativeClass) {
-                                    if(o.getValue() instanceof Objective) {
-                                        Objective objective = (Objective)o.getValue();
-                                        objectives.add(objective);
-                                    }
-                                    else {
-                                        Cons.println(((LeoNativeClass) o).getNativeClass() + " is not of type: " + Objective.class.getName());
-                                    }
-                                    
-                                }
-                            }
-                            break;
-                        }
-                        case NATIVE_CLASS: {
-                            LeoObject o = scriptedObjectives;
-                            if(o.getValue() instanceof Objective) {
-                                Objective objective = (Objective)o.getValue();
-                                objectives.add(objective);
-                            }
-                            else {
-                                Cons.println(((LeoNativeClass) o).getNativeClass() + " is not of type: " + Objective.class.getName());
-                            }
-                            break;
-                        }
-                        default: {
-                            Cons.println("*** ERROR -> objectives must either be an Array of objectives or a Java class or custom Leola class");
-                        }
-                    }
-                }
-                
-                LeoObject scriptedDefenders = config.getObject("defenders");
-                if(LeoObject.isTrue(scriptedDefenders)) {
-                    switch(scriptedDefenders.getType()) {
-                        case INTEGER:
-                        case LONG:
-                        case REAL:
-                            defenders = (byte)scriptedDefenders.asInt();
-                            break;
-                        case STRING:
-                            if(Team.ALLIED_TEAM_NAME.equalsIgnoreCase(scriptedDefenders.toString())) {
-                                defenders = Team.ALLIED_TEAM_ID;
-                            }                            
-                            break;
-                        default:{
-                            Cons.println("*** ERROR -> defenders must either be a 2(for allies) or 4(for axis) or 'allies' or 'axis' values");
-                        }
-                    }
-                }
-                
-                alliedSpawnPoints = loadSpawnPoint(config, "alliedSpawnPoints");
-                axisSpawnPoints = loadSpawnPoint(config, "axisSpawnPoints");
-                if(config.hasObject("minimumObjectivesToComplete")) {
-                    minimumObjectivesToComplete = config.getObject("minimumObjectivesToComplete").asInt();
-                }
-                else {
-                    minimumObjectivesToComplete = objectives.size();
-                }
-            }
-        }
-        
         final long timeBetweenRounds = 10_000L;
-        
-        GameType gameType = new ObjectiveGameType(getRuntime(), objectives, alliedSpawnPoints, axisSpawnPoints, 
+
+        File scriptFile = new File(mapFile + ".obj.leola");
+        if (!scriptFile.exists()) {
+            throw new NotExistScriptFileException();
+        };
+
+        LeoObject config = getRuntime().eval(scriptFile);
+        if (LeoObject.isTrue(config)) {
+            
+            addscriptedObjectives(objectives, config);
+            defenders = addScriptedDenfenders(defenders, config);
+            alliedSpawnPoints = loadSpawnPoint(config, "alliedSpawnPoints");
+            axisSpawnPoints = loadSpawnPoint(config, "axisSpawnPoints");
+            minimumObjectivesToComplete = checkMinumumObjectCompleteSize(objectives, config);
+            
+        }
+
+        return new ObjectiveGameType(getRuntime(), objectives, alliedSpawnPoints, axisSpawnPoints,
                 minimumObjectivesToComplete, maxScore, matchTime, timeBetweenRounds, defenders);
-        return gameType;
+        
+    }
+
+    private int checkMinumumObjectCompleteSize(List<Objective> objectives, LeoObject config) {
+        int minimumObjectivesToComplete;
+        if (config.hasObject("minimumObjectivesToComplete")) {
+            minimumObjectivesToComplete = config.getObject("minimumObjectivesToComplete").asInt();
+        } else {
+            minimumObjectivesToComplete = objectives.size();
+        }
+        return minimumObjectivesToComplete;
+    }
+
+    private void addscriptedObjectives(List<Objective> objectives, LeoObject config) throws Exception {
+        LeoObject scriptedObjectives = config.getObject("objectives");
+        if (LeoObject.isTrue(scriptedObjectives)) {
+            loadscriptObjectCase(objectives, scriptedObjectives);
+        }
+    }    
+    private void loadscriptObjectCase(List<Objective> objectives, LeoObject scriptedObjectives) throws Exception{
+        switch (scriptedObjectives.getType()) {
+        case ARRAY: {
+            addAllLeoObjectValues(objectives, scriptedObjectives);
+            break;
+        }
+        case NATIVE_CLASS: {
+            addNativeClassObjectValues(objectives, scriptedObjectives);
+            break;
+        }
+        default: {
+            throw new NonObjectTypeException();
+        }
+        }
+    }
+
+    private byte addScriptedDenfenders(byte defenders, LeoObject config) throws Exception {
+        LeoObject scriptedDefenders = config.getObject("defenders");
+        if (LeoObject.isTrue(scriptedDefenders)) {
+            defenders = loadscriptedDefenderCase(defenders, scriptedDefenders);
+        }
+        return defenders;
+    }
+
+    private byte loadscriptedDefenderCase(byte defenders, LeoObject scriptedDefenders)throws Exception {
+        switch (scriptedDefenders.getType()) {
+        case INTEGER:
+        case LONG:
+        case REAL:
+            defenders = (byte) scriptedDefenders.asInt();
+            break;
+        case STRING:
+            if (Team.ALLIED_TEAM_NAME.equalsIgnoreCase(scriptedDefenders.toString())) {
+                defenders = Team.ALLIED_TEAM_ID;
+            }
+            break;
+        default: {
+            throw new NonDefenderTypeException();
+        }
+        }
+        return defenders;
+    }
+
+
+    private void addAllLeoObjectValues(List<Objective> objectives, LeoObject scriptedObjectives) throws Exception {
+        LeoArray array = scriptedObjectives.as();
+        for (int i = 0; i < array.size(); i++) {
+            addLeoObjectValues(objectives, array, i);
+        }
+    }
+
+    private void addNativeClassObjectValues(List<Objective> objectives, LeoObject scriptedObjectives)throws Exception {
+        LeoObject o = scriptedObjectives;
+        addObjectValue(objectives, o);
+    }
+
+
+    private void addLeoObjectValues(List<Objective> objectives, LeoArray array, int i) throws Exception {
+        LeoObject o = array.get(i);
+        if (o instanceof LeoNativeClass) {
+            addObjectValue(objectives, o);
+        }
+    }
+    private void addObjectValue(List<Objective> objectives, LeoObject o) throws NonLeoNativeTypeException {
+        if (o.getValue() instanceof Objective) {
+            objectives.add((Objective)o.getValue());
+        } else {
+            printNonNativeClassTypeToConsole(o);
+            throw new NonLeoNativeTypeException();
+        }
+    }
+
+
+    private void printNonNativeClassTypeToConsole(LeoObject o) {
+        Cons.println(((LeoNativeClass) o).getNativeClass() + " is not of type: "+ Objective.class.getName());
     }
 }
