c01c2761697900c048b2c8db13f8da9bbadf9202
virginbabylon <ranya00@naver.com>
2018-06-09 13:26:34 +0900

    ai/basic/memory
    template method pattern
    strategy pattern

---
+++ src/seventh/ai/basic/memory/SightExpireStrategy.java
@@ -0,0 +1,22 @@
+package seventh.ai.basic.memory;
+
+import seventh.game.entities.PlayerEntity;
+import seventh.game.weapons.Weapon;
+import seventh.math.Vector2f;
+
+public class SightExpireStrategy implements ExpireStrategy {
+
+	protected PlayerEntity entity;
+	protected Weapon lastSeenWithWeapon;
+    protected Vector2f lastSeenAt;
+    protected boolean isValid;
+	@Override
+	public void expire() {
+		 this.entity = null;
+         this.lastSeenWithWeapon = null;
+         
+         this.lastSeenAt.zeroOut();                        
+         this.isValid = false;		
+	}
+
+}
