39df3a843f8ecc0557ed63e7d13f893c54259c67
tonysparks <anthony.sparks31@gmail.com>
2018-06-15 19:44:50 -0500

    updated version #

--- src/seventh/client/SeventhGame.java
+++ src/seventh/client/SeventhGame.java
@@ -71,7 +71,7 @@ public class SeventhGame implements ApplicationListener {
             "                                                                             \n" 
             ;
     
-    private static final String VERSION = "v0.3.0-BETA";
+    private static final String VERSION = "v0.4.0-BETA";
     public static final int DEFAULT_MINIMIZED_SCREEN_WIDTH  = 840; // 640//960;//1024;
     public static final int DEFAULT_MINIMIZED_SCREEN_HEIGHT = 480;//400//600;//768;
     
